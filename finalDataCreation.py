4ca2580fac088374483cbb96fc59def9d560a6a5
ermunroe <ermunroe@gmial.com>
2025-03-27 15:44:04 -0400

    Completely Aggregated Dataset to be used for NN.

--- finalDataCreation.py
+++ finalDataCreation.py
@@ -13,9 +13,10 @@ df7 = pd.read_csv('2020_merged.csv')
 df8 = pd.read_csv('2021_merged.csv')
 df9 = pd.read_csv('2022_merged.csv')
 df10 = pd.read_csv('2023_merged.csv')
+df11 = pd.read_csv('2024_merged.csv')
 
 #Merge all the data together
-df = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8, df9, df10], ignore_index=True)
+df = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8, df9, df10, df11], ignore_index=True)
 
 #Export the data
 df.to_csv('finalData.csv', index=False)
