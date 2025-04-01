b5370d8e97ff915d2be9d9bd99ce079927a33278
ermunroe <ermunroe@gmial.com>
2025-03-31 23:47:26 -0400

    Not sure why I had to do this.

---
+++ network.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from collections import Counter
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+
+
+
+df = pd.read_csv('finalData.csv')
+
+#Filter out data from 2014-2017 since the data is not complete for those years
+df_filtered = df[(df['season_x'] > 2017)]
+
+
+# Target Variables to include in the model
+targetVariables = ['race_lap_len', 'race_track_surface', 'race_track_name', 
+                   'finish_position', 'starting_position', 'driver_id', 'driver_name', 'driver_nationality', 'driver_number', 'race_laps_lead', 'points_earned', 'playoff_points_earned',
+                   'race_laps_run', 'race_status', 'race_cautions', 'race_speed', 'race_lead_changes', 'race_date', 'number_of_leaders', 'race_car_count', 'race_pole_time', 'race_miles_completed', 'race_purse_completed',
+                   'car', 'owner_id', 'sponsor', 'race_winner_name', 'race_winner_real_id', 'race_winner_starting_pos', 'race_winner_make', 'restrictor_plate', 'pole_winner_speed', 'race_speed', 'race_lap_len', 'number_of_caution_laps']
+
+
+# Filter the DataFrame to only include the target variables
+df_filtered['winner'] = df_filtered['finish_position'].apply(lambda x: 1 if x == 1 else 0)
+
+#Filter out NTT, DFP, and MQ from 'race_pole_time' column
+df_filtered['race_pole_time'] = df_filtered['race_pole_time'].replace(['NTT', 'DFP', 'MQ'], np.nan)
+
+# Define the X and y variables
+X_target = df_filtered[targetVariables]
+y = df_filtered['winner']
+
+
+# Categorical variables to include in the model
+X_categorical = X_target[['driver_nationality', 'driver_id', 'driver_name', 'sponsor', 'owner_id', 'car', 'race_track_surface', 'race_track_name']]
+encoder = OneHotEncoder(sparse_output=False)
+X_categorical = encoder.fit_transform(X_categorical)
+
+# Continuous variables to include in the model
+X_continuous = X_target[['race_pole_time', 'race_speed', 'race_speed', 'pole_winner_speed', 'race_lap_len']]
+scaler = StandardScaler()
+X_continuous = scaler.fit_transform(X_continuous)
+
+# Discrete variables to include in the model
+X_discrete = X_target[['finish_position', 'starting_position', 'driver_number', 'race_laps_run', 'points_earned', 'playoff_points_earned', 'race_cautions', 'race_lead_changes', 'number_of_leaders', 'number_of_caution_laps', 'race_laps_lead']]
+X_discrete = scaler.fit_transform(X_discrete)
+
+# Combine the categorical, continuous, and discrete variables into a single DataFrame
+X_stacked = np.concatenate((X_categorical, X_continuous, X_discrete), axis=1)
+X_stacked = pd.DataFrame(X_stacked)
+
+# Handle missing values
+imputer = SimpleImputer(strategy="mean")
+X_imputed = imputer.fit_transform(X_stacked)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.2, random_state=42)
+
+# Use SMOTE to handle class imbalance in the training set
+smote = SMOTE(random_state=42)
+X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+
+
+# Number of Nodes to have in the network
+print(X_imputed.shape[1])
+#3119 with these specific features
+
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
+
